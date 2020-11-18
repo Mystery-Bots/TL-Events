efd6ef612e95d61c589887b46f816ab95d8876cd
The-Mystery <the-mystery@outlook.com>
2020-11-19 11:18:25 +1300

    Moved: spawn stuff to database

--- services/spawn.js
+++ services/spawn.js
@@ -1,11 +1,6 @@
 const ms = require("ms")
 const Discord = require("eris-additions")(require("eris"))
 
-let minSpawn = 1
-let maxSpawn = 3 // (This is actually the number below this)
-
-let SpawnTime = 1604867889
-
 typeConvert = {
     0: "Common",
     1: "Rare",
@@ -21,7 +16,6 @@ async function getType(){
     }else if (35 <= num ){
         type = 0
     }
-    console.log(num)
     return type
 }
 
@@ -29,6 +23,9 @@ function spawnGift(type){
     let embed = {
         title:`A ${type} Gift Appeared`,
         description: `Use \`tlclaim\` to claim the gift`,
+        image: {
+            url: "https://discord.com/assets/7a7ff35f227d659eb5efd817e2052174.svg"
+        },
         color: 0xafafaf
     }
     return embed
@@ -37,7 +34,7 @@ function spawnGift(type){
 function unclaimedGift(type){
     let embed = {
         title:`A ${type} Gift Disappeared`,
-        description: `The Gift has disappeared because was unclaimed for longer than 5 minutes`,
+        description: `The Gift has disappeared due to being unclaimed for longer than 5 minutes`,
     }
     return embed
 } 
@@ -52,81 +49,114 @@ function claimedGift(type, user){
 } 
 
 module.exports.Run = async function(bot, message){
+    let statsCollection = bot.database.collection('stats')
+    let statsResult = await statsCollection.findOne({_id:'5fb5896be09eb535b97403be'})
     let now = Date.now()
     if (message.author.bot) return
-    if (now > SpawnTime){
+    if (now > statsResult.SpawnTime){
         let channel = bot.getChannel("633920642605121578")
         let type = typeConvert[await getType()]
         let spawnMessage = await channel.createMessage({embed: spawnGift(type)})
-        let newSpawnDuration = Math.floor(Math.random() * (3 - 1) + 1) // Mins
-        SpawnTime = (Date.now() + ms(`${newSpawnDuration}m`)).toFixed(0)
-        let responses = await channel.awaitMessages(m => m.content === "tlclaim", { time: ms('30s'), maxMatches: 1 });    
+        let newSpawnDuration = Math.floor(Math.random() * ((statsResult.maxSpawn+1) - statsResult.minSpawn) + 1) // Mins
+        let SpawnTime = (Date.now() + ms(`${newSpawnDuration}s`)).toFixed(0)
+        await statsCollection.updateOne({_id:'5fb5896be09eb535b97403be'},{$set:{'SpawnTime':SpawnTime}})
+        let responses = await channel.awaitMessages(m => m.content === "tlclaim", { time: ms('5m'), maxMatches: 1 });    
         if (responses.length){
-            await responses[0].delete()
-            const collection = bot.database.collection(`${message.channel.guild.id}`)
-            console.log(collection)
-            let result = await collection.findOne({"userID": `${responses[0].author.id}`})
-            if (!result){
+            try{
+                await responses[0].delete()
+            }
+            catch (e) {
+                await spawnMessage.delete()
+                return message.channel.createMessage("Gift removed due to error")
+            }
+            let userCollection = bot.database.collection(`${message.channel.guild.id}`)
+            let userResult = await userCollection.findOne({"userID": `${responses[0].author.id}`})
+            if (!userResult){
                 if (type == "Common"){
-                    updateDoc = {
+                    userUpdateDoc = {
                         "userID": responses[0].author.id,
                         "username":`${responses[0].author.username}#${responses[0].author.discriminator}`,
-                        "Common": "1",
-                        "Rare": "0",
-                        "Epic": "0"
+                        "common": 1,
+                        "rare": 0,
+                        "epic": 0,
+                        "totalPoints":1
                     }
+                    statsUpdateDoc = {$inc:{
+                        "totalClaims": 1,
+                        "totalCommon": 1
+                    }}
                 }
                 else if (type == "Rare"){
-                    updateDoc = {
+                    userUpdateDoc = {
                         "userID": responses[0].author.id,
                         "username":`${responses[0].author.username}#${responses[0].author.discriminator}`,
-                        "Common": "1",
-                        "Rare": "0",
-                        "Epic": "0"
+                        "common": 0,
+                        "rare": 1,
+                        "epic": 0,
+                        "totalPoints":2
                     }
+                    statsUpdateDoc = {$inc:{
+                        "totalClaims": 1,
+                        "totalRare": 1
+                    }}
                 }
                 else if (type == "Epic"){
-                    updateDoc = {
+                    userUpdateDoc = {
                         "userID": responses[0].author.id,
                         "username":`${responses[0].author.username}#${responses[0].author.discriminator}`,
-                        "Common": "1",
-                        "Rare": "0",
-                        "Epic": "0"
+                        "common": 0,
+                        "rare": 0,
+                        "epic": 1,
+                        "totalPoints":5
                     }
+                    statsUpdateDoc = {$inc:{
+                        "totalClaims": 1,
+                        "totalEpic": 1
+                    }}
                 }
-                console.log(type)
-                await collection.insertOne(updateDoc); 
+                await statsCollection.updateOne({_id:'5fb5896be09eb535b97403be'},statsUpdateDoc)
+                await userCollection.insertOne(userUpdateDoc); 
             }
             else{
-                console.log(result)
-                console.log(result[type])
-                count = parseInt(result[type]) +1
-                console.log(typeof(count))
-                console.log(count)
+                count = userResult[type.toLowerCase()] +1
                 if (type == "Common"){
-                    updateDoc = {
+                    userUpdateDoc = {
                         $set:{
-                            Common:`${count}` 
+                            common: count,
+                            totalPoints: (userResult.totalPoints + 1)
                         }
                     }
+                    statsUpdateDoc = {$inc:{
+                        "totalClaims": 1,
+                        "totalCommon": 1
+                    }}
                 }
                 else if (type == "Rare"){
-                    updateDoc = {
+                    userUpdateDoc = {
                         $set:{
-                            Rare:`${count}` 
+                            rare: count,
+                            totalPoints: (userResult.totalPoints + 2)
                         }
                     }
+                    statsUpdateDoc = {$inc:{
+                        "totalClaims": 1,
+                        "totalRare": 1
+                    }}
                 }
                 else if (type == "Epic"){
-                    updateDoc = {
+                    userUpdateDoc = {
                         $set:{
-                            Epic:`${count}` 
+                            epic: count,
+                            totalPoints: (userResult.totalPoints + 5)
                         }
                     }
+                    statsUpdateDoc = {$inc:{
+                        "totalClaims": 1,
+                        "totalEpic": 1
+                    }}
                 }
-                
-                console.log(updateDoc)
-                await collection.updateOne({userID: responses[0].author.id}, updateDoc); 
+                await statsCollection.updateOne({_id:'5fb5896be09eb535b97403be'},statsUpdateDoc)
+                await userCollection.updateOne({userID: responses[0].author.id}, userUpdateDoc); 
             }
             spawnMessage.edit({embed: claimedGift(type, responses[0].author.mention)})
         }else{
