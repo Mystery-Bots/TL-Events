c4b7151e4b292c5737e317108aeaf15f9ce089a5
The-Mystery <the-mystery@outlook.com>
2020-12-02 11:51:54 +1300

    Added: Staff lock and messaged (test mode)

--- commands/Staff/mute.js
+++ commands/Staff/mute.js
@@ -1,6 +1,7 @@
 const ms = require('ms')
 
 module.exports.run = async (bot, message, args) => {
+    if (!message.member.roles.includes('704921933073875014')) return message.channel.createMessage("Sorry but you can not use this command.")
     user = message.mentions[0]
     if (!user){
         userArray = await message.channel.guild.fetchMembers({userIDs:[args[0]]})
@@ -8,8 +9,10 @@ module.exports.run = async (bot, message, args) => {
     }
     eventChannel = bot.getChannel("779081002311352370")
     eventChannel.editPermission(user.id, 0,2048,"member","Event mute")
+    eventChannel.createMessage(`User: ${user.username}#${user.discriminator} has been muted`)
     setTimeout(() => {
         eventChannel.deletePermission(user.id, "Event unmute")
+        eventChannel.createMessage(`User: ${user.username}#${user.discriminator} has been unmuted`)
     }, ms("20s"));
     
 }
