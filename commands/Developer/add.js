12d3718edcd5d56206db82cc3140b6a7bbacd552
TheMystery <the-mystery@outlook.com>
2021-01-19 12:03:59 +1300

    Added: add command
    Used to add and removed eggs from a users profile.

---
+++ commands/Developer/add.js
@@ -0,0 +1,15 @@
+module.exports.run = async (bot, message, args) => {
+    if (!bot.config.devs.includes(message.author.id)) return console.log(`${message.author.username} (ID: ${message.author.id}) tried to use "add"`)
+    let user = args[0]
+    let playerCollection = bot.database.collection("players")
+    let statsCollection = bot.database.collection("stats")
+    await playerCollection.updateOne({"userID":user}, {$inc:{"staffEggs":parseInt(args[1])}})
+    await statsCollection.updateOne({"_id":"600608c92fe331ec1a128a1f"}, {$inc:{"staffEggs":parseInt(args[1])}})
+    message.channel.createMessage({content:`Added ${args[1]} eggs to <@${user}>'s profile`, messageReferenceID:message.id})
+}
+
+module.exports.info = {
+    name: "add",
+    description: "Add eggs to a users profile",
+    category: "Developer"
+}
