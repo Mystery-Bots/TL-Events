987ba9e7a2639071ef27dde43c54abb0d803b9f8
TheMystery <the-mystery@outlook.com>
2021-01-19 22:07:29 +1300

    Updated: New leaderboards
    Overall: The main leaderboard for the event
    
    Found: The leaderboard for the top found edds (secret eggs)
    
    Collected: Leaderboard for most collected eggs
    
    Streak: Leaderboard for the longest streak

--- commands/Game/leaderboard.js
+++ commands/Game/leaderboard.js
@@ -1,30 +1,115 @@
-medals = {
+const EmbedPaginator = require('eris-pagination');
+
+let medals = {
     "1":"🥇",
     "2":"🥈",
     "3":"🥉"
 }
 
-module.exports.run = async (bot, message, args) => {
-    let guild = message.channel.guild
-    let collection = bot.database.collection(`${guild.id}`)
-    let top10 = await collection.find().sort({totalPoints:-1}).limit(10).toArray()
-    let descriptionList = ["Based on total points:"]
+async function overallLeaderboard(collection){
+    let top10 = await collection.find().sort({totalEggs:-1}).limit(10).toArray()
+    let descriptionList = []
     let placing = 1
     top10.forEach(user => {
-        descriptionList.push(`${medals[placing] ? medals[placing] : `#${placing}`} <@${user.userID}>: ${user.totalPoints.toLocaleString()}`)
+        descriptionList.push(`${medals[placing] ? medals[placing] : `#${placing}`} <@${user.userID}>: ${user.totalEggs.toLocaleString()}`)
         placing += 1
     })
-    embedObject = {
-        title:"Leaderboard Top 10",
+    let embedObject = {
+        author:{
+            name: "Leaderboard Top 10"
+        },
+        title: "Overall Leaderboard",
         description: descriptionList.join('\n'),
-        color: 0xf2ae4f
+        color: 0xf2ae4f,
+        footer: {
+            text: "Page 1/4"
+        }
     }
-    message.channel.createMessage({embed:embedObject})
+    return embedObject
+}
+
+async function foundLeaderboard(collection){
+    let top10 = await collection.find().sort({foundEggs:-1}).limit(10).toArray()
+    let descriptionList = []
+    let placing = 1
+    top10.forEach(user => {
+        descriptionList.push(`${medals[placing] ? medals[placing] : `#${placing}`} <@${user.userID}>: ${user.foundEggs.toLocaleString()}`)
+        placing += 1
+    })
+    let embedObject = {
+        author:{
+            name: "Leaderboard Top 10"
+        },
+        title: "Found Leaderboard",
+        description: descriptionList.join('\n'),
+        color: 0xf2ae4f,
+        footer: {
+            text: "Page 2/4"
+        }
+    }
+    return embedObject
+}
+
+async function collectedLeaderboard(collection){
+    let top10 = await collection.find().sort({collectedEggs:-1}).limit(10).toArray()
+    let descriptionList = []
+    let placing = 1
+    top10.forEach(user => {
+        descriptionList.push(`${medals[placing] ? medals[placing] : `#${placing}`} <@${user.userID}>: ${user.collectedEggs.toLocaleString()}`)
+        placing += 1
+    })
+    let embedObject = {
+        author:{
+            name: "Leaderboard Top 10"
+        },
+        title: "Collected Leaderboard",
+        description: descriptionList.join('\n'),
+        color: 0xf2ae4f,
+        footer: {
+            text: "Page 3/4"
+        }
+    }
+    return embedObject
+}
+
+async function streakLeaderboard(collection){
+    let top10 = await collection.find().sort({streak:-1}).limit(10).toArray()
+    let descriptionList = []
+    let placing = 1
+    top10.forEach(user => {
+        descriptionList.push(`${medals[placing] ? medals[placing] : `#${placing}`} <@${user.userID}>: ${user.streak.toLocaleString()}`)
+        placing += 1
+    })
+    let embedObject = {
+        author:{
+            name: "Leaderboard Top 10"
+        },
+        title: "Streak Leaderboard",
+        description: descriptionList.join('\n'),
+        color: 0xf2ae4f,
+        footer: {
+            text: "Page 4/4"
+        }
+    }
+    return embedObject
+}
+
+module.exports.run = async (bot, message, args) => {
+    let overall = await overallLeaderboard(bot.database.collection("players"))
+    let found = await foundLeaderboard(bot.database.collection("players"))
+    let collected = await collectedLeaderboard(bot.database.collection("players"))
+    let streak = await streakLeaderboard(bot.database.collection("players"))
+    EmbedPaginator.createPaginationEmbed(message, [overall, found, collected, streak], {
+        showPageNumbers: false,
+        extendedButtons: true
+    });
 }
 
 module.exports.info = {
     name: "leaderboard",
     description: "Get the current leaderboard",
     category: "Game",
-    cooldown: "20s"
+    cooldown: "20s",
+    GuildOnly: true,
+    aliases: ["lb"]
 }
