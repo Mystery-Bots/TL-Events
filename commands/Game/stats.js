5a2e04c3ed40ac17e69ea2a7d9005fdab6cfb318
TheMystery <the-mystery@outlook.com>
2021-01-24 13:52:30 +1300

    Updated: for displaying easter player stats

--- commands/Game/stats.js
+++ commands/Game/stats.js
@@ -1,66 +1,52 @@
+const moment = require("moment")
+
 medals = {
-    "1":"🥇",
-    "2":"🥈",
-    "3":"🥉"
-}
-
-
+	1: "🥇",
+	2: "🥈",
+	3: "🥉",
+};
 
 module.exports.run = async (bot, message, args) => {
-    let template = {
-        "common": 0,
-        "rare":0,
-        "epic":0,
-        "totalPoints":0
-    }
-    const collection = bot.database.collection(`${message.channel.guild.id}`)
-    user = message.mentions[0]
-    if(!user){
+	let collection = bot.database.collection("players");
+	if (!args[0]) {
+		user = message.member;
+	} else {
+		userArray = await message.channel.guild.fetchMembers({
+			userIDs: [args[0]],
+		});
+		user = userArray[0];
+	}
+	let userResult = await collection.findOne({ userID: `${user.id}` });
+	if (!userResult) {
         if (!args[0]){
-            user = message.author
-        }
-        else {
-            userArray = await message.channel.guild.fetchMembers({userIDs:[args[0]]})
-            user = userArray[0]
+            return message.channel.createMessage("Run `tlclaim` to claim your first batch of eggs")
+        }else {
+            return message.channel.createMessage("No player profile found")
         }
     }
-    let userResult = await collection.findOne({"userID": `${user.id}`})
-    if (!userResult) {
-        template.userID = user.id,
-        template.username = `${user.username}#${user.discriminator}`
-        await collection.insertOne(template); 
-        userResult = await collection.findOne({"userID": `${user.id}`})
-    }
-    let allResult = await collection.find().sort({totalPoints:-1}).toArray()
-    placing = (allResult.map((e)=>{return e.userID}).indexOf(user.id))+1
-    embed = {
-        title: `${user.username}'s Stats`,
-        color: 0xff99f8,
-        description: `ID: ${user.id}\n\n**Total Points:** ${userResult.totalPoints.toLocaleString()}\n**Ranking:** ${placing}${medals[placing] ? medals[placing] : ""}`,
-        fields: [
-            {
-                name: "Common (1 pt)",
-                value: userResult.common.toLocaleString()
-            },
-            {
-                name: "Rare (2 pts)",
-                value: userResult.rare.toLocaleString()
-            },
-            {
-                name: "Epic (5 pts)",
-                value: userResult.epic.toLocaleString()
-            },
-            {
-                name: "Total Gifts",
-                value: (userResult.common+userResult.rare+userResult.epic).toLocaleString()
-            }
-        ]
-    }
-    message.channel.createMessage({embed:embed})
-}
+	let allResult = await collection.find().sort({ totalEggs: -1 }).toArray();
+	placing =
+		allResult
+			.map((e) => {
+				return e.userID;
+			})
+			.indexOf(user.id) + 1;
+	embed = {
+		title: `${user.username}'s Stats`,
+		color: user.color,
+		description: `ID: ${user.id}\n**Ranking:** ${placing}${
+			medals[placing] ? medals[placing] : ""
+        }\n\n**Total Eggs:** ${userResult.totalEggs.toLocaleString()}\nCollected Eggs: ${userResult.collectedEggs.toLocaleString()}\nFound Eggs: ${userResult.foundEggs.toLocaleString()}\nStaff Eggs: ${userResult.staffEggs.toLocaleString()}\n\nStreak Total: ${userResult.streak.toLocaleString()}`,
+        footer: {
+            text:"Last collected"
+        },
+        timestamp: moment(userResult.lastCollected)
+	};
+	message.channel.createMessage({ embed: embed });
+};
 
 module.exports.info = {
-    name: "stats",
-    description: "Get stats on a player.",
-    category: "Game",
-}
+	name: "stats",
+	description: "Get stats on a player.",
+	category: "Game",
+};
