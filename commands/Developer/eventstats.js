14357c2b522dd881578fb6236ac1c4c387d49582
TheMystery <the-mystery@outlook.com>
2021-01-24 13:50:20 +1300

    Updated: for new easter stats and information

--- commands/Developer/eventstats.js
+++ commands/Developer/eventstats.js
@@ -1,61 +1,52 @@
 module.exports.run = async (bot, message, args) => {
-    if (!bot.config.devs.includes(message.author.id)) return console.log(`${message.author.username} (ID: ${message.author.id}) tried to use "eventstats"`)
-    let collection = bot.database.collection('stats')
-    let result = await collection.findOne({_id:'5fb5896be09eb535b97403be'})
-    let embed = {
-        title: "Christmas Event Stats",
-        fields: [
-            {
-                name: "Spawn Times (Minutes)",
-                value: `Min: ${result.minSpawn}\nMax: ${result.maxSpawn}`,
-                inline: true
-            },
-            {
-                name: "\u200B",
-                value: "\u200B",
-                inline: true
-            },
-            {
-                name: "\u200B",
-                value: "\u200B",
-                inline: true
-            },
-            {
-                name: "Total Christmas Gifts (Epic Gifts)",
-                value: `${result.totalChristmas.toLocaleString()}`,
-                inline: true
-            },
-            {
-                name: "Total Staff Gifts",
-                value: `${result.totalStaff.toLocaleString()}`,
-                inline: true
-            },
-            {
-                name: "Total Collected Gifts",
-                value: `${result.totalClaims.toLocaleString()}`,
-                inline: true
-            },
-            {
-                name: "Total Common Gifts",
-                value: `${result.totalcommon.toLocaleString()}`,
-                inline: true
-            },
-            {
-                name: "Total Rare Gifts",
-                value: `${result.totalrare.toLocaleString()}`,
-                inline: true
-            },
-            {
-                name: "Total Epic Gifts",
-                value: `${result.totalepic.toLocaleString()}`,
-                inline: true
-            },
-        ]}
-    await message.channel.createMessage({embed:embed})
-}
+	if (!bot.config.devs.includes(message.author.id))
+		return console.log(
+			`${message.author.username} (ID: ${message.author.id}) tried to use "eventstats"`
+		);
+	let collection = bot.database.collection("stats");
+	let result = await collection.findOne({ _id: "600608c92fe331ec1a128a1f" });
+	let embed = {
+		title: "Easter Event Stats",
+		fields: [
+			{
+				name: "Total Staff Eggs",
+				value: `${result.staffEggs.toLocaleString()}`,
+				inline: true,
+			},
+			{
+				name: "Total Found Eggs",
+				value: `${result.foundEggs.toLocaleString()}`,
+				inline: true,
+			},
+			{
+				name: "\u200B",
+				value: "\u200B",
+				inline: true,
+			},
+			{
+				name: "Times Collected",
+				value: `${result.timesCollected.toLocaleString()}`,
+				inline: true,
+			},
+			{
+				name: "Total Collected Eggs",
+				value: `${result.collectedEggs.toLocaleString()}`,
+				inline: true,
+			},
+			{
+				name: "\u200B",
+				value: "\u200B",
+				inline: true,
+			},
+		],
+		color:0x42b3f5
+	};
+	await message.channel.createMessage({ embed: embed });
+};
 
 module.exports.info = {
-    name: "eventstats",
-    description: "Get all the stats about the event.",
-    category: "Developer",
-}
+	name: "eventstats",
+	description: "Get all the stats about the event.",
+	category: "Developer",
+	aliases: ["es"],
+};
