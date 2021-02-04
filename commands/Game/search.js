6f55e96aa1bcf30bca8c13b38b48d292cb421f4f
TheMystery <the-mystery@outlook.com>
2021-02-04 19:38:53 +1300

    Fixed: Floor the halfed egg counts

--- commands/Game/search.js
+++ commands/Game/search.js
@@ -8,9 +8,9 @@ module.exports.run = async (bot, message, args) => {
 	if (chance <= 70){
 		let eggs = Math.floor(Math.random() * (30 - 10 + 1) + 10);
 		if (user.passive){
-			await collection.updateOne({"userID":message.author.id},{$inc:{"collectedEggs":(eggs/2)}})
-			await collection.updateOne({"userID":message.author.id},{$inc:{"totalEggs":(eggs/2)}})
-			return message.channel.createMessage(`Congrats managed to find ${eggs/2} eggs`)
+			await collection.updateOne({"userID":message.author.id},{$inc:{"collectedEggs":(Math.floor(eggs/2))}})
+			await collection.updateOne({"userID":message.author.id},{$inc:{"totalEggs":(Math.floor(eggs/2))}})
+			return message.channel.createMessage(`Congrats managed to find ${Math.floor(eggs/2)} eggs`)
 		}else{
 			await collection.updateOne({"userID":message.author.id},{$inc:{"collectedEggs":(eggs)}})
 			await collection.updateOne({"userID":message.author.id},{$inc:{"totalEggs":(eggs)}})
