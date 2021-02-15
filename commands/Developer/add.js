e4fe7bee254656c91897fd1d7228e7af937bf835
TheMystery <the-mystery@outlook.com>
2021-02-15 18:19:47 +1300

    Fixed: totaleggs not being updated

--- commands/Developer/add.js
+++ commands/Developer/add.js
@@ -3,7 +3,7 @@ module.exports.run = async (bot, message, args) => {
     let user = args[0]
     let playerCollection = bot.database.collection("players")
     let statsCollection = bot.database.collection("stats")
-    await playerCollection.updateOne({"userID":user}, {$inc:{"staffEggs":parseInt(args[1])}})
+    await playerCollection.updateOne({"userID":user}, {$inc:{"staffEggs":parseInt(args[1]),"totalEggs":parseInt(args[1])}})
     await statsCollection.updateOne({"_id":"600608c92fe331ec1a128a1f"}, {$inc:{"staffEggs":parseInt(args[1])}})
     message.channel.createMessage({content:`Added ${args[1]} eggs to <@${user}>'s profile`, messageReferenceID:message.id})
 }
